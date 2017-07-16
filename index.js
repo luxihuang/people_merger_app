7be52d5f680e11d289839a3c1fe5f45d84b29b57
luxihuang <Luxi.Huang@gmail.com>
2017-07-16 00:09:07 -0400

    writing files keep on overwriting

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 const fs = require('fs');
+// const mainFile = './peopleComplete.json';
+var files = ['./people1.json', './people2.json',];
 
-fs.readFile('./people1.json', 'utf8', (err, data) => {
-    if(err) throw err;
-    console.log(data);
+function read(file){
+    fs.readFile(file, 'utf8', (err, data) => {
+        if(err) throw err;
+        console.log(data);
 
-      fs.writeFile('peopleComplete.txt', data, function(err){
-        if (err) { //check for possible errors 
-            console.log(`there was a problem writing: ${err}`); //log that an error happened
-            //throw the error for handling by the caller
-            throw err;
-            console.log('content from  been written.');
-        }
-    })
-});
+        fs.writeFile('./peopleComplete.txt', data, function(err){
+            if (err) throw err;
+                var results = console.log(data + 'written data');
+                results.sort();
+            })
+        })
+};
 
-fs.readFile('./people2.json', 'utf8', (err, data) => {
-    if(err) throw err;
-    console.log(data);
+for(var i = 0; i <files.length; i++){
+    read(files[i]);
+    console.log(files[i]);   
+};
+     
 
-      fs.writeFile('peopleComplete.txt', data, function(err){
-        if (err) { //check for possible errors 
-            console.log(`there was a problem writing: ${err}`); //log that an error happened
-            //throw the error for handling by the caller
-            throw err;
-            console.log('content from  been written.');
-        }
-    })
-});
+
+// function readAppend(mainFile, fileToBeAppended) {
+//     fs.readFile(fileToBeAppended, 'utf8', function (err, data) {
+//         if(err) throw err;
+//         var results1 = data;
+//         console.log(results);
+
+//     fs.appendFile(mainFile, data, (err) => {
+//       if (err) throw err;
+//       // console.log('The "data to append" was appended to file');
+//     });
+//     });
+// }
+
+// // for(var i = 0; i < fileToBeAppended.length; i++){
+// //     read(fileToBeAppended[i]);
+// //     console.log(fileToBeAppended[i]);   
+// // };
+
+
+// fileToBeAppended = './people1.json';
+// readAppend(mainFile, fileToBeAppended);
+
+// fileToBeAppended = './people2.json';
+// readAppend(mainFile, fileToBeAppended);
+
+
+//source --> http://stevehanov.ca/blog/index.php?id=127
+//source --> https://stackoverflow.com/questions/34943132/node-js-read-two-files-compare-parts-of-files-and-output-a-sorted-file
